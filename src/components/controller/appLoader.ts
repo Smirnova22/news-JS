f7cd35932c895efb149765e95844d99361ce913e
Anastasia Smirnova <shvetsova22@mail.ru>
2024-02-23 23:14:12 +0200

    feat: apploader become .ts

---
+++ src/components/controller/appLoader.ts
@@ -0,0 +1,23 @@
+interface EnvironmentVariables {
+    API_URL: string;
+    API_KEY?: string;
+  }
+  
+  declare const process: {
+    env: EnvironmentVariables;
+  };
+  
+
+
+  import Loader from './loader';
+
+  class AppLoader extends Loader {
+    constructor() {
+      super(process.env.API_URL, {
+        apiKey: process.env.API_KEY,
+      });
+    }
+  }
+  
+  export default AppLoader;
+  
